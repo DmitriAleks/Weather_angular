a1e4b0ca239b3abad9038529f354138f46f40fd4
Dmitri Aleks <alexeyonok.dmitriy@neatsoft.io>
2022-05-21 15:08:57 +0300

    request

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,31 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpModule } from '../services/httpt.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {HttpModule} from '../services/httpt.service';
+import {catchError, mergeMap, Subject, Subscription, takeUntil, throwError} from "rxjs";
+
+type StatusType = 'intro' | 'loader' | 'success' | 'error'
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
-
+export class MainComponent implements OnInit, OnDestroy {
+  showLoader = false
   searchCityName = ''
-  lon = ''
-  lan =''
+  longitude = 1
+  latitude = 1
+  currentTemp!: number
+  status: StatusType = 'intro'
+  city!: string
 
-  constructor(private httpRequest: HttpModule ) { }
+  // private destroy$ = new Subject<void>();
+  private subscription = new Subscription();
+
+  constructor(private httpRequest: HttpModule) {
+  }
+
 
   ngOnInit(): void {
   }
-  onSeacrh(){
-    this.httpRequest.getCoordinatesByLocationName(this.searchCityName).subscribe(response =>{
-      // this.lan=response.lan
-      // this.lon = response.lon
-      console.log(response);
 
 
-      
-    })
-  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    // this.destroy$.next();
+    // this.destroy$.complete();
+  }
+
+  onSearch() {
+    this.showLoader = true
+    this.status = 'loader'
+    this.subscription.add(this.httpRequest.getCoordinatesByLocationName(this.searchCityName)
+      .pipe(
+        mergeMap((response) => {
+            if (!response?.length) {
+              return throwError(response);
+            }
+
+            this.latitude = response[0].lat
+            this.longitude = response[0].lon
+            return this.httpRequest.getWeatherByCoordinate(response[0].lat, response[0].lon)
+          }
+        ),
+        catchError((err) => {
+            console.log(err);
+            this.status = 'error'
+            return throwError(err);
+          },
+        ),
+        ).subscribe(
+      response => {
+
+        console.log(response)
+        console.log('latitude', this.latitude)
+        this.showLoader = false
+        this.currentTemp = response.main.temp
+        this.searchCityName = ''
+        this.city = response.name
+        this.status = 'success'
+      }))
   }
 
 }
--- src/app/services/httpt.service.ts
+++ src/app/services/httpt.service.ts
@@ -1,29 +1,57 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
 
 
-interface Weather {
-    country: string
-lat: 51.5073219
-local_names: any
-lon: string
-name: string
-state:string
+interface CoordinatesResponse {
+  country: string
+  lat: number
+  local_names: any
+  lon: number
+  name: string
+  state: string
+}
+
+interface WeatherResponse {
+  base: string
+  clouds: {all: number}
+  cod: number
+  coord: {lon: number, lat: number}
+  dt: number
+  id: number
+  main: {
+    feels_like: number
+    grnd_level: number
+    humidity: number
+    pressure: number
+    sea_level: number
+    temp: number
+    temp_max: number
+    temp_min: number
+    name: string
+  }
+  name: "Minsk City"
+  sys: {type: number, id: number, country: string, sunrise: number, sunset: number}
+  timezone: number
+  visibility: number
+  weather: [{id: number, main: string, description: string, icon: string}]
+  wind: {speed: number, deg: number, gust: number}
 }
 
 @Injectable({providedIn: 'root'})
 
 
 export class HttpModule {
-    
 
-    constructor(private http: HttpClient){}
 
-    getCoordinatesByLocationName(localName: string){
-        return this.http.get(`http://api.openweathermap.org/geo/1.0/direct?q=${localName}&appid=6512c45d086c4a87938f4eb18dda0056`)
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    getWeater(lat:string,lon:string,){
-        return this.http.get(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=6512c45d086c4a87938f4eb18dda0056`)
-    }
+  getCoordinatesByLocationName(localName: string): Observable<CoordinatesResponse[]> {
+    return this.http.get<CoordinatesResponse[]>(`http://api.openweathermap.org/geo/1.0/direct?q=${localName}&appid=6512c45d086c4a87938f4eb18dda0056`)
+  }
+
+  getWeatherByCoordinate(lat: number, lon: number):Observable<WeatherResponse> {
+    return this.http.get<WeatherResponse>(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6512c45d086c4a87938f4eb18dda0056&units=metric&lang=ru`)
+  }
 }
